ba4cda2b85c90c57971162b189c77bf24a4027bc
Bianca Oria <biancaoria@gmail.com>
2025-10-30 19:01:33 -0300

    ajustando curva de aprendizagem Percepto Simples e Adaline

---
+++ Adaline/Adaline.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ADALINE:
+    def __init__(self,X_train,y_train,learning_rate=1e-2,max_epoch=3,tol=1e-5,plot=True):
+        self.p, self.N = X_train.shape
+        self.X_train = np.vstack((
+            -np.ones((1,self.N)), X_train
+        ))
+        self.max_epoch = max_epoch
+        self.tol = tol
+        self.d = y_train.flatten()
+        self.lr = learning_rate
+        self.w = np.zeros((self.p+1,1))
+        self.w = np.random.random_sample((self.p+1,1))-.5
+        self.plot = plot
+        self.x1 = 0
+        self.errors_per_epoch = [] 
+        if plot:
+            # plt.ion() # ?
+            self.fig = plt.figure(2)
+            self.ax = self.fig.add_subplot()
+            self.ax.scatter(self.X_train[1,self.d[:]==1],
+                            self.X_train[2,self.d[:]==1],marker='s',s=120)
+            self.ax.scatter(self.X_train[1,self.d[:]==-1],
+                            self.X_train[2,self.d[:]==-1],marker='o',s=120)
+            margin = 1  # margem extra
+            x_min, x_max = self.X_train[1].min() - margin, self.X_train[1].max() + margin
+            y_min, y_max = self.X_train[2].min() - margin, self.X_train[2].max() + margin
+            
+
+            self.ax.set_xlim(x_min, x_max)
+            self.ax.set_ylim(y_min, y_max)
+            
+            
+            self.x1 = np.linspace(x_min,x_max)
+            
+            self.ax.set_xlabel("Variável 1")
+            self.ax.set_ylabel("Variável 2")
+    
+            self.ax.set_title("Plano de Regressão do ADALINE")
+            # self.ax.legend()
+            
+            
+        
+    def draw_line(self,c,alpha,lw=2):
+        
+        x2 = -self.w[1,0]/self.w[2,0]*self.x1 + self.w[0,0]/self.w[2,0]
+        x2 = np.nan_to_num(x2)
+        plt.plot(self.x1,x2,c=c,alpha=alpha)
+        
+    def activation_function(self, u):
+        return 1 if u>=0 else -1
+    
+    def EQM(self):#conferido pelo psudocodigo
+        eqm = 0
+        for k in range(self.N):
+            x_k = self.X_train[:,k].reshape(self.p+1,1)
+            u_k = (self.w.T@x_k)[0,0]
+            d_k = self.d[k]
+            eqm += (d_k-u_k)**2
+        return eqm/(2*self.N)
+    def fit(self):
+        epochs = 0
+        EQM1 = 0
+        EQM2 = 1
+        while epochs < self.max_epoch :
+            EQM1 = self.EQM()
+            self.errors_per_epoch.append(EQM1)
+            for k in range(self.N):
+                x_k = self.X_train[:,k].reshape(self.p+1,1)
+                u_k = (self.w.T@x_k)[0,0] #produto escalar
+                d_k = self.d[k]#saida desejada
+                e_k = d_k-u_k #erro quadratico
+                self.w = self.w + self.lr*e_k*x_k #lr taxa de aprendizagem
+            
+            epochs+=1
+            EQM2 = self.EQM()#até aqui ok, conferigo pelo pseudo codigo
+            # plt.pause(.1)
+            
+        self.draw_line(c='b',alpha=1)
+        self.errors_per_epoch.append(EQM2)
+        # # plt.pause(.1)
+        # plt.show(block=True)
+        # plt.figure(5)
+        # plt.plot(self.errors_per_epoch)
+        # plt.grid()
+        # plt.title("Curva de Aprendizagem (ADALINE)")
+        # plt.show()
+    def predict(self, X_test):
+            
+        p_test, N_test = X_test.shape
+        
+        X_test_bias = np.vstack((
+            -np.ones((1, N_test)), X_test
+        ))
+        
+        u_test = self.w.T @ X_test_bias
+        
+        y_pred = [self.activation_function(u) for u in u_test.flatten()]
+            
+        return np.array(y_pred).reshape(-1, 1)
+        
+        
+
+
