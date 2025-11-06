a9ad00da1ed3e1c4336d019326457b230a5a2ab2
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-11-06 16:58:36 -0300

    refatoracao cores

--- 1_Fase/PerceptronMult/Perceptron_Mult.py
+++ 1_Fase/PerceptronMult/Perceptron_Mult.py
@@ -28,9 +28,9 @@ class Perceptron_Mult:
             self.fig = plt.figure(2)
             self.ax = self.fig.add_subplot()
             self.ax.scatter(self.X_train[1, self.d[:, 0] == 1],
-                            self.X_train[2, self.d[:, 0] == 1],c='r', marker='s', s=120, edgecolor='k')
+                            self.X_train[2, self.d[:, 0] == 1],c='#ff2cc9', marker='s', s=120, edgecolor='k')
             self.ax.scatter(self.X_train[1, self.d[:, 0] == -1],
-                            self.X_train[2, self.d[:, 0] == -1],c='b', marker='o', s=120, edgecolor='k')
+                            self.X_train[2, self.d[:, 0] == -1],c='#8e51ff', marker='o', s=120, edgecolor='k')
             margin = 0
             x_min, x_max = self.X_train[1].min() - margin, self.X_train[1].max() + margin
             y_min, y_max = self.X_train[2].min() - margin, self.X_train[2].max() + margin
